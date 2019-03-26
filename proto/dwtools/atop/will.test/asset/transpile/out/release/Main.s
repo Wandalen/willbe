22c2cf7715510cc0b89676467f8b1ecffa5dc9b2
Wandalen <wandalen.me@gmail.com>
2019-03-26 16:51:08 +0200

    auto commit

--- proto/dwtools/atop/will.test/asset/transpile/out/release/Main.s
+++
@@ -1,3 +0,0 @@
-// ======================================
-console.log("dir2/File.s"), console.log("dir2/File1.debug.js"), console.log("dir2/File1.release.js"), 
-console.log("dir2/File2.debug.js"), console.log("dir2/File2.release.js");
--- proto/dwtools/atop/will.test/asset/transpile/out/tests.compiled.release/Tests.s
+++
@@ -1,2 +0,0 @@
-// ======================================
-console.log("dir2/File.test.s"), console.log("dir3.test/File.test.s");
