618c2f15f74929af0c30158ac9cba12bb3e821cf
Volodymyr M <volodymyr.migdal@gmail.com>
2019-10-17 20:56:47 +0300

    repair unctionPlatform

--- proto/dwtools/atop/will.test/WillExternals.test.s
+++ proto/dwtools/atop/will.test/WillExternals.test.s
@@ -11273,7 +11273,8 @@ function functionPlatform( test )
     platform = 'osx'
 
     test.identical( got.exitCode, 0 );
-    test.identical( _.strCount( got.output, /\+ reflector::copy reflected 2 file\(s\) .+functionPlatform.* : \.\/out\/dir.windows <- \.\/proto in .*/), 1 );
+    test.identical( _.strCount( got.output, '+ reflector::copy reflected 2 file(s)' ), 1 );
+    test.identical( _.strCount( got.output, `./out/dir.${platform} <- ./proto in` ), 1 );
 
     var files = self.find( outPath );
 
