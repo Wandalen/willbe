295ae6d3c070231f8438d9c4293f2c900261ef16
Volodymyr Migdal <volodymyr.migdal@gmail.com>
2020-02-14 13:42:41 +0200

    adjust hookPrepare

--- proto/dwtools/atop/will.test/Ext.test.s
+++ proto/dwtools/atop/will.test/Ext.test.s
@@ -5219,9 +5219,9 @@ function hookPrepare( test )
   test.is( true );
 
   let config = _.fileProvider.fileConfigUserRead();
-  let user = config.about.user;
   if( !config || !config.about || !config.about[ 'github.token' ] )
   return null;
+  let user = config.about.user;
 
   /* - */
 
