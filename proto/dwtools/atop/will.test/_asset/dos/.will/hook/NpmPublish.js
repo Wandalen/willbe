1636b00b2b0496f54bc5b27585e25cee28cfc47a
wandalen <wandalen.me@gmail.com>
2020-05-01 17:04:30 +0300

    .

--- proto/dwtools/atop/will.test/_asset/dos/.will/hook/NpmPublish.js
+++ proto/dwtools/atop/will.test/_asset/dos/.will/hook/NpmPublish.js
@@ -49,7 +49,7 @@ function onModule( it )
 
   _.assert( _.path.isTrailed( it.junction.localPath ), 'not tested' );
 
-  it.start( 'will .export' );
+  it.start( 'local-will .export' ); /* xxx */
 
   let activeСonfigPath = wasСonfigPath;
   if( !o.dry )
