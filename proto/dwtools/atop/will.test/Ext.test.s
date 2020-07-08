6205ae688b1cba7f60e79275ecb58d17ac2a458b
Dmytro <dm.vict.kr@gmail.com>
2020-07-08 15:36:18 +0300

    Fix test routine `cleanRecursiveMin`, use new submodules structure

--- proto/dwtools/atop/will.test/Ext.test.s
+++ proto/dwtools/atop/will.test/Ext.test.s
@@ -15682,6 +15682,7 @@ function cleanRecursiveMin( test )
       './group1/out',
       './group1/out/a.out.will.yml',
       './group1/out/debug',
+      './group1/out/debug/Integration.test.s',
       './group1/out/debug/dwtools',
       './group1/out/debug/dwtools/testing',
       './group1/out/debug/dwtools/testing/Basic.s',
@@ -15699,6 +15700,7 @@ function cleanRecursiveMin( test )
       './out',
       './out/z.out.will.yml',
       './out/debug',
+      './out/debug/Integration.test.s',
       './out/debug/dwtools',
       './out/debug/dwtools/testing',
       './out/debug/dwtools/testing/Basic.s',
@@ -15718,7 +15720,7 @@ function cleanRecursiveMin( test )
       './out/debug/dwtools/testing/l3/testing1b/Include.s',
       './out/debug/dwtools/testing/l3/testing1b/ModuleForTesting1b.s',
       './out/debug/dwtools/testing/l3.test',
-      './out/debug/dwtools/testing/l3.test/ModuleForTesting1b.test.s',
+      './out/debug/dwtools/testing/l3.test/ModuleForTesting1b.test.s'
     ];
     var files = a.findNoModules( a.routinePath );
     test.identical( files, exp );
