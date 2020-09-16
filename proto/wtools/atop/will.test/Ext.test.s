eaaa2dbe62c89c893c7f1c9d0c412a1b6647d146
Dmytro <dm.vict.kr@gmail.com>
2020-09-16 14:27:48 +0300

    Improve test routines `*GitPrOpen`, use system config for bot

--- proto/wtools/atop/will.test/Ext.test.s
+++ proto/wtools/atop/will.test/Ext.test.s
@@ -26531,6 +26531,13 @@ function commandSubmodulesGitPrOpen( test )
   let a = context.assetFor( test, 'git-push' );
   a.reflect();
 
+  let config = _.censor !== undefined ? _.censor.configRead() : a.fileProvider.configUserRead();
+  if( !config || !config.about || config.about.user !== 'wtools-bot' )
+  {
+    test.is( true );
+    return;
+  }
+
   /* - */
 
   a.appStartNonThrowing( '.with original/Git.* .submodules.git.pr.open "some title" srcBranch:new' )
@@ -27191,6 +27198,13 @@ function commandModulesGitPrOpen( test )
   let context = this;
   let a = context.assetFor( test, 'git-push' );
   a.reflect();
+
+  let config = _.censor !== undefined ? _.censor.configRead() : a.fileProvider.configUserRead();
+  if( !config || !config.about || config.about.user !== 'wtools-bot' )
+  {
+    test.is( true );
+    return;
+  }
 
   /* - */
 
@@ -27932,6 +27946,13 @@ function commandGitPrOpen( test )
   let a = context.assetFor( test, 'git-push' );
   a.reflect();
 
+  let config = _.censor !== undefined ? _.censor.configRead() : a.fileProvider.configUserRead();
+  if( !config || !config.about || config.about.user !== 'wtools-bot' )
+  {
+    test.is( true );
+    return;
+  }
+
   /* - */
 
   a.appStartNonThrowing( '.with original/Git.* .git.pr.open "some title" srcBranch:new' )
@@ -28018,8 +28039,7 @@ function commandGitPrOpenRemote( test )
   a.reflect();
 
   /* - */
-
-  let config = a.fileProvider.configUserRead();
+  let config = _.censor !== undefined ? _.censor.configRead() : a.fileProvider.configUserRead();
   if( !config || !config.about || !config.about[ 'github.token' ] )
   {
     test.is( true );
