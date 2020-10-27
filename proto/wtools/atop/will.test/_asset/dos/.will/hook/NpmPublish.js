9eac9c725e5106baf28f9cf65bad3cae24876e7d
wandalen <wandalen.me@gmail.com>
2020-10-27 09:24:21 +0200

    .

--- proto/wtools/atop/will.test/_asset/dos/.will/hook/NpmPublish.js
+++ proto/wtools/atop/will.test/_asset/dos/.will/hook/NpmPublish.js
@@ -94,7 +94,7 @@ function onModule( context )
 
   _.assert( path.isTrailed( context.junction.localPath ), 'not tested' );
 
-  context.start( 'local-will .export' ); /* xxx */
+  context.start( 'will .export' ); /* xxx */
 
   let activeСonfigPath = wasСonfigPath;
   if( !o.dry )
