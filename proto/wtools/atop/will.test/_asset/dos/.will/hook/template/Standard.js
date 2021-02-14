d0113dc5d6db8b14185de77c3439ba78c24dc7f6
wandalen <wandalen.me@gmail.com>
2021-02-14 22:18:13 +0200

    .

--- proto/wtools/atop/will.test/_asset/dos/.will/hook/template/Standard.js
+++ proto/wtools/atop/will.test/_asset/dos/.will/hook/template/Standard.js
@@ -1242,7 +1242,7 @@ package-lock.json
 `,
 
   'LICENSE' : //
-`Copyright (c) 2013-2020 {:about/full.name:}({:about/org:})
+`Copyright (c) 2013-2021 {:about/full.name:}({:about/org:})
 
 Permission is hereby granted, free of charge, to any person
 obtaining a copy of this software and associated documentation
