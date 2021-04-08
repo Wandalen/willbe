fb8ac17deb431dd915996bcddbc8e7e1cd5de028
wandalen <wandalen.me@gmail.com>
2021-04-08 12:01:30 +0300

    version 0.5.383

--- proto/wtools/atop/will/l4/Module.s
+++ proto/wtools/atop/will/l4/Module.s
@@ -9542,7 +9542,8 @@ function prList( o )
   {
     if( o.logger && o.logger.verbosity )
     if( op.result )
-    op.result.forEach( ( pr ) => o.logger.log( `${pr.title} ${_.ct.format( 'from', 'secondary' )} ${pr.head.user.login}` ) );
+    op.result.forEach( ( pr ) => o.logger.log( `${pr.description.head} ${_.ct.format( 'from', 'secondary' )} ${pr.user.name}` ) );
+    // op.result.forEach( ( pr ) => o.logger.log( `${pr.title} ${_.ct.format( 'from', 'secondary' )} ${pr.head.user.login}` ) );
     // o.logger.log( `ABC${_.ct.format( 'ABC', 'secondary' )}ABC` )
     return op;
   });
