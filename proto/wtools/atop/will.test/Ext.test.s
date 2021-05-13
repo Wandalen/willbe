c1585a9f740fbc0dec35b2117f8262c5f0b98a99
dmvict <dm.vict.kr@gmail.com>
2021-05-13 09:02:42 +0300

    Fix time out for routine `commandsSequenceProceduresTermination`, 200% of original time out

--- proto/wtools/atop/will.test/Ext.test.s
+++ proto/wtools/atop/will.test/Ext.test.s
@@ -42585,7 +42585,7 @@ function commandsSequenceProceduresTermination( test )
 }
 
 commandsSequenceProceduresTermination.rapidity = 1;
-commandsSequenceProceduresTermination.routineTimeOut = 30000;
+commandsSequenceProceduresTermination.routineTimeOut = 60000;
 commandsSequenceProceduresTermination.description =
 `
 Runs two commands in sequence.
