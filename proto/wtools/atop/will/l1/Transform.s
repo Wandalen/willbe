b6fd14219a380efcaed4ff066108bca5c12e6c8f
dmvict <dm.vict.kr@gmail.com>
2021-05-18 16:47:53 +0300

    Implement routine `authorRecordNormalize`

--- proto/wtools/atop/will/l1/Transform.s
+++ proto/wtools/atop/will/l1/Transform.s
@@ -10,8 +10,94 @@ const Self = _.will.transform = _.will.transform || Object.create( null );
 // implementation
 // --
 
+function authorRecordNormalize( src )
+{
+  _.assert( arguments.length === 1, 'Expects exactly one argument' );
+
+  let result = src;
+  if( _.str.is( src ) )
+  {
+    let validAuthorString = authorRecordVerify( src );
+    if( !validAuthorString )
+    throw _.err( `Author record::${ src } is not valid` );
+  }
+  else if( _.aux.is( src ) )
+  {
+    result = src.name;
+    if( src.email )
+    result += ` <${ src.email }>`;
+    if( src.url )
+    result += ` (${ src.url })`;
+    return result;
+  }
+  else
+  {
+    _.assert( 0, 'Unexpected type of record' );
+  }
+
+  return result;
+
+  /* */
+
+  function authorRecordVerify()
+  {
+    let recordIsValid = false;
+    let splits = split( src );
+    if( splits[ 1 ] !== undefined )
+    {
+      _.assert( splits[ 0 ] !== '', 'Expects author' );
+
+      if( uriIs( splits[ 2 ] ) )
+      splits = split( splits[ 0 ] );
+
+      if( splits[ 1 ] === undefined )
+      recordIsValid = authorVerify( splits[ 0 ] );
+      else if( emailIs( splits[ 2 ] ) )
+      recordIsValid = authorVerify( splits[ 0 ] );
+      else
+      recordIsValid = authorVerify( splits.join( ' ' ) );
+    }
+    else
+    {
+      recordIsValid = authorVerify( src );
+    }
+    return recordIsValid;
+  }
+
+  /* */
+
+  function split( src ) /* Dmytro : should I write it inplace? */
+  {
+    return _.strIsolateRightOrAll({ src, delimeter : /\s+/ });
+  }
+
+  /* */
+
+  function uriIs( uri )
+  {
+    return /\(\S+\)/.test( uri );
+  }
+
+  /* */
+
+  function emailIs( email )
+  {
+    return /\<\S+\>/.test( email );
+  }
+
+  /* */
+
+  function authorVerify( author )
+  {
+    return !/(<|>|\(|\))/.test( author ) && author !== '';
+  }
+}
+
+//
+
 function submodulesSwitch( src, enabled )
 {
+  _.assert( arguments.length === 2, 'Expects exactly two arguments' );
   _.assert( _.aux.is( src ), 'Expects aux like config' );
   _.assert( _.bool.like( enabled ), 'Expects bool value to switch submodule' );
 
@@ -46,6 +132,7 @@ let Extension =
 {
 
   submodulesSwitch,
+  authorRecordNormalize,
 
 }
 
