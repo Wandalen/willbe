977b58893257faa6778acc8512aa75285716d9ca
dmvict <dm.vict.kr@gmail.com>
2021-08-18 14:26:58 +0300

    Extend asset `stepSourcesJoin` by files for testing in browser

--- proto/wtools/atop/will.test/_asset/stepSourcesJoin/Single.s
+++ proto/wtools/atop/will.test/_asset/stepSourcesJoin/Single.s
@@ -1,5 +1,11 @@
 
-module.exports.sumOfAll = function ( a, b )
+_global_.Test = Object.create( null );
+
+function sumOfAll( a, b )
 {
   return a + b;
 }
+
+_global_.Test.sumOfAll = sumOfAll;
+
+module.exports = _global_.Test;
