e28d050c61f7429db0b5e0bbe439959d932c61db
dmvict <dm.vict.kr@gmail.com>
2021-08-18 14:27:23 +0300

    Write test routine `stepSourcesJoinRunInBrowser`

--- proto/wtools/atop/will.test/Ext.test.s
+++ proto/wtools/atop/will.test/Ext.test.s
@@ -24853,6 +24853,40 @@ function stepSourcesJoin( test )
 
 //
 
+function stepSourcesJoinRunInBrowser( test )
+{
+  let context = this;
+  let a = context.assetFor( test, 'stepSourcesJoin' );
+  a.reflectMinimal();
+
+  /* - */
+
+  a.appStart( '.build join.module.for.browser' );
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
+    test.identical( _.strCount( op.output, 'Building module::stepSourcesJoin / build::join.module.for.browser' ), 1 );
+    test.identical( _.strCount( op.output, 'Built module::stepSourcesJoin / build::join.module.for.browser' ), 1 );
+    test.identical( _.strCount( op.output, 'Built module::stepSourcesJoin / build::join.module.for.browser in' ), 1 );
+    return null;
+  });
+  a.shell( 'npm i jsdom@17.0.0' );
+  a.shell( 'node Browser.s' );
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
+    var exp = 'The sum of sum, multiplication, squares, square roots and dividing of 4 and 4 is';
+    test.identical( _.strCount( op.output, exp ), 1 );
+    return null;
+  });
+
+  /* - */
+
+  return a.ready;
+}
+
+//
+
 function stepNpmGenerate( test )
 {
   let context = this;
@@ -44645,6 +44679,7 @@ const Proto =
     stepShellWithPathResolving,
     stepShellWithSeveralCommands,
     stepSourcesJoin,
+    stepSourcesJoinRunInBrowser,
     stepNpmGenerate,
     stepNpmGenerateOptionsInStep,
     stepGitCheckHardLinkRestoring,
