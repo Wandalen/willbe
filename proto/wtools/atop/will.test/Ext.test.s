cec5ef33a87657ca279977cdaa3285841e2baf3d
dmvict <dm.vict.kr@gmail.com>
2021-11-24 13:04:23 +0200

    Fix test routine `exportWithRemoteSubmodules`, run command with submodules

--- proto/wtools/atop/will.test/Ext.test.s
+++ proto/wtools/atop/will.test/Ext.test.s
@@ -13214,18 +13214,17 @@ function exportWithRemoteSubmodules( test )
   a.appStart( '.with group1/b .clean' );
   a.appStart( '.with group2/c .clean' );
   a.appStart( '.with group1/group10/a0 .export' );
-  a.appStart( '.with group1/a .export' );
-  a.appStart( '.with group1/b .export' );
-  a.appStart( '.with group2/c .export' );
-  a.appStart( '.with z .export' );
-
+  a.appStart( '.imply withSubmodules:1 .with group1/a .export' );
+  a.appStart( '.imply withSubmodules:1 .with group1/b .export' );
+  a.appStart( '.imply withSubmodules:2 .with group2/c .export' );
+  a.appStart( '.imply withSubmodules:2 .with z .export' );
   a.ready.then( ( op ) =>
   {
     test.identical( op.exitCode, 0 );
     test.identical( _.strCount( op.output, 'Failed to open' ), 1 );
     test.identical( _.strCount( op.output, '. Opened .' ), 24 );
-    test.identical( _.strCount( op.output, '+ 1/4 submodule(s) of module::z were downloaded' ), 1 );
-    test.identical( _.strCount( op.output, '+ 0/4 submodule(s) of module::z were downloaded' ), 0 );
+    test.identical( _.strCount( op.output, '+ 1/8 submodule(s) of module::z were downloaded' ), 1 );
+    test.identical( _.strCount( op.output, '+ 0/4 submodule(s) of module::z were downloaded' ), 1 );
 
     var exp =
     [
